614d83434610db286856545122013a3e6182be24
Rensley <87101559+doble196@users.noreply.github.com>
2023-06-06 11:26:31 -0400

    added hello, newyear,tasks

--- src/Django/lecture3/lecture3/urls.py
+++ src/Django/lecture3/lecture3/urls.py
@@ -1,7 +1,7 @@
-"""Django URL Configuration
+"""lecture3 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", include("hello.urls")),
+    path("admin/", admin.site.urls),
+    path("hello/", include("hello.urls")),
+    path("tasks/", include("tasks.urls")),
+    path("newyear/", include("newyear.urls")),
 ]
